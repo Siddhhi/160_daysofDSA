56cc64bd9b0ec16b4fc2b69a9272195b7d36c65e
Siddhhi <siddhi041216@gmail.com>
2025-04-03 22:10:05 -1200

    Update approach(1).cpp

--- Day 043/approach(1).cpp
+++ Day 043/approach(1).cpp
@@ -3,7 +3,6 @@
         Count Pairs with a Given Sum (Brute Force Approach)
     Problem Statement:
         - Given an array of integers, count the number of pairs (i, j) where arr[i] + arr[j] equals a target sum.
-
     Returns:
         - The number of pairs that satisfy the condition.
 
