a211ed7b59127e97c8255a98e820916b32cc8118
Siddhhi <siddhi041216@gmail.com>
2025-04-13 22:34:12 +0530

    Update optimized-approach.cpp

--- Day 047/optimized-approach.cpp
+++ Day 047/optimized-approach.cpp
@@ -4,7 +4,7 @@
     Problem Statement:
         - Given an array of integers, find the length of the longest consecutive subsequence.
 
-    Author: Vishal Singhaniya
+    Author: Siddhi
 */
 
 #include <iostream>
