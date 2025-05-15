878b70c588a7994697e9229e80319021491df08b
Siddhhi <siddhi041216@gmail.com>
2025-05-14 16:20:21 -1200

    Update brute-force-approach.cpp

--- Day 062/brute-force-approach.cpp
+++ Day 062/brute-force-approach.cpp
@@ -5,11 +5,7 @@
         - Given an array of integers and an integer k,
           find the length of the longest subarray whose sum equals k.
 
-    Approach:
-        - For every possible subarray, calculate its sum.
-        - If the sum equals k, update the maximum length.
-        - Brute-force method using two nested loops.
-
+ 
     Time Complexity:
         - **O(n²)** — Two nested loops.
 
