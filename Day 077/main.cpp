2b30c49246292e18a9185c35c708155f3b85f357
Siddhhi <siddhi041216@gmail.com>
2025-06-10 13:59:52 +0530

    Update main.cpp

--- Day 077/main.cpp
+++ Day 077/main.cpp
@@ -1,21 +1,12 @@
 /*
-    Title: N-Queens Problem using Backtracking
+    Title: N-Queens Problem using Backtracking.
 
     Problem Statement:
         - Place N queens on an N x N chessboard such that no two queens threaten each other.
         - A queen can move any number of squares vertically, horizontally, or diagonally.
         - Print all possible arrangements where N queens can be safely placed.
 
-    Approach:
-        - Use backtracking to explore all safe positions for placing queens column-wise.
-        - Use three boolean arrays to keep track of attacks:
-            1. rows[i]      → if a queen is in row i
-            2. diag1[i-j+n] → major diagonal
-            3. diag2[i+j]   → minor diagonal
-        - On placing a queen, mark attacks and recurse for next column.
-        - Backtrack by unmarking the position.
 
-    Time Complexity:
         - O(N!), exponential, since we try all possibilities.
 
     Space Complexity:
