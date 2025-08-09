ced34f7a55000f629ae4efc8f049d8aae193b7ec
Siddhhi <siddhi041216@gmail.com>
2025-08-08 23:57:36 -1200

    Update main.cpp

--- Day 113/main.cpp
+++ Day 113/main.cpp
@@ -6,17 +6,6 @@
         - A subsequence is derived from another string by deleting some or no elements 
           without changing the order of the remaining characters.
 
-   .
-        - Base cases:
-            - If low > high → length is 0.
-            - If low == high → length is 1.
-        - Recurrence:
-            - If s[low] == s[high], add 2 to result of lps(low+1, high-1).
-            - Else, take the maximum between lps(low+1, high) and lps(low, high-1).
-        - Use a 2D memoization table to avoid recomputing states.
-
-    Time Complexity:
-        - O(n^2), due to overlapping subproblems in recursion.
 
     Space Complexity:
         - O(n^2), for memoization storage.
@@ -62,4 +51,5 @@ int main() {
          << longestPalinSeq(s);
     return 0;
 }
+
 
