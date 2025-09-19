632264699a50876d1d3e530f78ab133ad32e1676
Siddhhi <siddhi041216@gmail.com>
2025-09-18 23:58:54 -1200

    Update main.cpp

--- Day 145/main.cpp
+++ Day 145/main.cpp
@@ -14,36 +14,9 @@ vector<vector<int>> constructAdj(int V, vector<vector<int>>& edges) {
     return res;
 }
 
-vector<int> articulationPoints(int V, vector<vector<int>>& edges) {
-    vector<vector<int>> adj = constructAdj(V, edges);
-    vector<int> res;
-    
-    // For each vertex, check if it's an articulation point
-    for(int i = 0; i < V; i++) {
-        vector<bool> visited(V, false);
-        visited[i] = true; // Remove vertex i
-        
-        int comp = 0;
-        
-        // Count connected components after removing vertex i
-        for(int j = 0; j < V; j++) {
-            if(j != i && !visited[j]) {
-                comp++;
-                dfs(j, adj, visited);
-            }
-        }
-        
-        // If removing vertex i increases number of components, it's articulation point
-        if(comp > 1) {
-            res.push_back(i);
-        }
-    }
-    
-    if(res.empty()) {
-        return {-1};
-    }
-    
-    sort(res.begin(), res.end());
+
+in(), res.end());
     return res;
 
 }
+
