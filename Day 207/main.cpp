5a40c00bd6ca844056bacca24d231a4747ab354e
Siddhhi <siddhi041216@gmail.com>
2025-11-30 22:28:05 -1200

    Update main.cpp

--- Day 207/main.cpp
+++ Day 207/main.cpp
@@ -15,36 +15,6 @@
                 9 → wxyz
         - Digits 0 and 1 do not map to any characters and should be skipped.
 
-    Example:
-        Input:
-            arr = {3, 6, 4}
-        Key Mapping:
-            3 → d e f
-            6 → m n o
-            4 → g h i
-
-        Output:
-            All possible combinations:
-            dmg dmh dmi dng dnh dni dog doh doi
-            emg emh emi eng enh eni eog eoh eoi
-            fmg fmh fmi fng fnh fni fog foh foi
-
-    Approach (Backtracking):
-        - Use recursion to explore all possible characters for each digit.
-        - For each digit:
-              → fetch all characters mapped to it,
-              → append one character to current prefix,
-              → move to next digit,
-              → backtrack by removing the last character.
-        - When we reach the end of the digit array, we push the formed string into result.
-
-    Dry Run:
-        arr = {3,6}
-        - Start with empty prefix ""
-        - For '3': d, e, f
-            - For '6': m, n, o
-            → dm, dn, do, em, en, eo, fm, fn, fo
-
     Time Complexity:
         - O(4^N), because each digit can represent up to 4 characters.
         - N = number of digits.
@@ -103,3 +73,4 @@ int main(){
     
     return 0;
 }
+
